19b60d9658aef30320c876ced0686b815f93aa8b
Pau <pau.lorca.moreso@gmail.com>
2024-02-27 15:22:10 +0100

    primera versió feta

---
+++ src/main/java/com/bepo04/interestingitems/InterestingItems.java
@@ -0,0 +1,8 @@
+package com.bepo04.interestingitems;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(InterestingItems.MODID)
+public class InterestingItems {
+    public static final String MODID = "interestingitems";
+}
